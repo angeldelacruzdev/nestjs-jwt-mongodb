b934fcfa4f6c974130a6c9ffccaa9acb8bc6f912
Angel De La Cruz <angeldelacruzdev@gmail.com>
2022-06-05 19:15:58 -1200

    Update package.json to new lates version

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/tracar_demo')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest')],
 })
 export class DataBaseModule {}
