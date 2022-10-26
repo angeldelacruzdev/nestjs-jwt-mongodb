b285d8081dcbc316a237276c81419dcc829499cb
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2022-10-25 20:07:24 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://peni:chupalasuave73@cluster0.vtqbc.mongodb.net/test')],
+  imports: [MongooseModule.forRoot('')],
 })
 export class DataBaseModule {}
